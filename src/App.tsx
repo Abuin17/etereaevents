e0adf40e8fb8a6ea27213811ada6b683c3b904aa
Abuin17 <manuabuingarcia@gmail.com>
2025-05-04 19:57:30 +0200

    fix: remove unused useRef import to fix Vercel build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import './styles/main.scss';
 import Navbar from './components/Navbar/Navbar';
